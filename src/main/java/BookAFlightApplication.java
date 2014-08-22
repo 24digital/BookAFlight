720493721d94fdd6a083dd10bb7f5eda1a9f537a
24digital <24digital@gmail.com>
2014-08-22 10:12:52 -0400

    Initial commit

---
+++ src/main/java/BookAFlightApplication.java
@@ -0,0 +1,14 @@
+import org.springframework.context.annotation.Configuration;
+import org.springframework.boot.SpringApplication;
+/**
+ * Created by Marion on 8/22/2014.
+ */
+
+@Configuration
+public class BookAFlightApplication {
+
+    public static void main(String a)
+    {
+        SpringApplication.run(BookAFlightApplication.class, a);
+    }
+}
